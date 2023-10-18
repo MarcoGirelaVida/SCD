59a0afa0354df113f1cf6f2abc5f5a4208bb3a58
Marco_Girela <marcogirelavida@gmail.com>
2023-10-18 10:54:52 +0200

    ProdCons Exercice Started

--- P1/prodcons_exe.cpp
+++ P1/prodcons_exe.cpp
@@ -17,8 +17,13 @@ const unsigned
 unsigned  
    cont_prod[num_items] = {0}, // contadores de verificación: para cada dato, número de veces que se ha producido.
    cont_cons[num_items] = {0}, // contadores de verificación: para cada dato, número de veces que se ha consumido.
-   siguiente_dato       = 0 ;  // siguiente dato a producir en 'producir_dato' (solo se usa ahí)
+   siguiente_dato       = 0 ,  // siguiente dato a producir en 'producir_dato' (solo se usa ahí)
+   primero_libre = 0 ,
+   ultimo_ocupado = 0 ,
+   buffer[tam_vec] = {0};
 
+Semaphore libres = tam_vec;
+Semaphore ocupadas = 0;
 //**********************************************************************
 // funciones comunes a las dos soluciones (fifo y lifo)
 //----------------------------------------------------------------------
@@ -44,7 +49,44 @@ void consumir_dato( unsigned dato )
 
 }
 
+void mostrar_buffer()
+{
+   // Pintar flecha de primero libre
+   for (int i = 1; i < tam_vec; i++)
+   {
+      cout << "   ";
+   }
+   cout << "  V Primero Libre" << endl;
 
+   // Pintar ralla superior
+   for (int i = 0; i < tam_vec; i++)
+   {
+      cout << "___";
+   }
+   cout << endl;
+   
+   // Pintar contenido del buffer
+   for (int i = 0; i < tam_vec; i++)
+   {
+      cout << "|" << buffer[i];
+   }
+   cout << "|" << endl;
+
+   // Pintar ralla inferior
+   for (int i = 0; i < tam_vec; i++)
+   {
+      cout << "___";
+   }
+   cout << endl;
+   
+   // Pintar flecha de ultimo ocupado
+   for (int i = 1; i < tam_vec; i++)
+   {
+      cout << "   ";
+   }
+   cout << "  A Ultimo ocupado" << endl;
+   
+}
 //----------------------------------------------------------------------
 
 void test_contadores()
@@ -72,7 +114,11 @@ void  funcion_hebra_productora(  )
    for( unsigned i = 0 ; i < num_items ; i++ )
    {
       int dato = producir_dato() ;
-      // completar ........
+      libres.sem_wait();
+      buffer[primero_libre] = dato;
+      primero_libre = (primero_libre + 1) % tam_vec;
+      ocupadas.sem_signal();
+      mostrar_buffer();
    }
 }
 
@@ -83,7 +129,10 @@ void funcion_hebra_consumidora(  )
    for( unsigned i = 0 ; i < num_items ; i++ )
    {
       int dato ;
-      // completar ......
+      ocupadas.sem_wait();
+      dato = buffer[ultimo_ocupado];
+      ultimo_ocupado = (ultimo_ocupado + 1) % tam_vec;
+      libres.sem_signal();
       consumir_dato( dato ) ;
     }
 }
